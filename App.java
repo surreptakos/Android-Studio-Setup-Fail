1bcca3d5ca23cde7fb36e9ac266ccc36ee56c5f4
surreptakos <djgatsakos@gmail.com>
2015-07-30 20:28:43 -0500

    Android Setup Fail (took an hour)

---
+++ App.java
@@ -0,0 +1,13 @@
+public class App {
+
+	public static void main(String[] args) {
+		// Tried to install Android Studio on my desktop and failed for about an
+		// hour
+		// It took up too much space on the C:\ Drive, which it shouldn't have
+		// been using in the first place since I installed it on D. I tried to
+		// move everything to D using Junction Links but it fucked up the
+		// install and I just uninstalled it. I'll try again tomorrow
+
+	}
+
+}
